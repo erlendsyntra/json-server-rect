34ab42cc23bf67f05dcc531f0cb078649ae730b6
Erlend Geerts <erlen@lightning.local>
2023-10-15 18:29:43 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+import randomColor from "randomcolor";
+import "/css/style.scss";
+
+async function main() {
+  const container = document.querySelector("#container");
+  const basePath = "http://localhost:3000/rectangles/";
+  render();
+
+  container.ondblclick = async (e) => {
+    const id = e.target.dataset.id;
+    const response = await fetch(basePath + id, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    });
+    render();
+  };
+
+  container.onclick = async (e) => {
+    const id = e.target.dataset.id;
+    const response = await fetch(basePath + id, {
+      method: "PATCH",
+      body: JSON.stringify({
+        color: randomColor(),
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    e.target.style.backgroundColor = data.color;
+    // document.querySelector(`[data-id="${data.id}"]`).style.backgroundColor = data.color;
+  };
+
+  async function render() {
+    const response = await fetch(basePath);
+    const data = await response.json();
+    container.innerHTML = data
+      .map(({ id, color, width, height, x, y }) => {
+        return `<div class="rectangle" data-id="${id}" style="background-color:${color}; width:${width}; height:${height}; top:${y}; left:${x}; position:absolute"></div>`;
+      })
+      .join("");
+  }
+}
+
+main();
